015d7ca4b63faa51cb57d19b2f451fe185bb2a98
rajasekhar.vanjarapu <rajasekhar.vanjarapu@qwikcilver.intra>
2018-12-07 17:09:09 +0530

    Added test framework

---
+++ Thrones/Thrones.Test/KingdomTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Thrones.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NUnit.Framework;
+    using Thrones.Core.Abstract;
+    using Thrones.Core.Entities;
+
+    [TestClass]
+    public class KingdomTests
+    {
+
+        public KingdomTests()
+        {
+        }
+
+        [TestCase("Ice", "Mammoth", "Do not waster paper", false)]
+        public void WillGiveSupportTests(string name, string emblem, string message, bool expectedResult)
+        {
+            IKingdom iceKingdom = KingdomFactory.CreateKingdom(name, emblem);
+        }
+    }
+}
